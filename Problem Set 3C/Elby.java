3ee715391859ff03338529452d5dd2b21c4908ed
charmeleonn <zhangpoki@gmail.com>
2024-10-11 13:29:34 -0700

    Add files via upload

---
+++ Problem Set 3C/Elby.java
@@ -0,0 +1,168 @@
+public class Elby {
+    /**
+     * Get a default greeting
+     *
+     * @return a greeting
+     */
+    public String getGreeting() {
+        return "Hello, let's talk.";
+    }
+
+    
+    /**
+ * Search for one word in phrase. The search is not case sensitive. This method
+ * will check that the given goal is not a substring of a longer string (so,
+ * for example, "I know" does not contain "no").
+ *
+ * @param statement the string to search
+ * @param goal the string to search for
+ * @param startPos the character of the string to begin the search at
+ * @return the index of the first occurrence of goal in statement or -1 if it's
+ * 	not found
+ */
+
+public int findKeyword(String statement, String goal, int startPos) {
+  String phrase = statement.trim().toLowerCase();
+  goal = goal.toLowerCase();
+
+  // The only change to incorporate the startPos is in the line below
+  int goalPos = phrase.indexOf(goal, startPos);
+
+  // Refinement--make sure the goal isn't part of a word
+  while (goalPos >= 0) {
+     	// Find the string of length 1 before and after the word
+     	String before = " ", after = " ";
+
+     	if (goalPos > 0) {
+     	   before = phrase.substring(goalPos - 1, goalPos);
+      }
+
+      if (goalPos + goal.length() < phrase.length()) {
+         after = phrase.substring(goalPos + goal.length(), 
+                                  goalPos + goal.length() + 1);
+      }
+
+      /* determine the values of goalPos, before, and after at this point */
+
+      // If before and after aren't letters, we've found the word
+      if (((before.compareTo("a") < 0) || (before.compareTo("z") > 0)) &&                
+          ((after.compareTo("a")  < 0) || (after.compareTo("z")  > 0))) {
+         return goalPos;
+      }
+
+      // The last position didn't work, so let's find the next, if there is one.
+      goalPos = phrase.indexOf(goal,goalPos + 1);
+   }
+   return -1;
+}
+
+/**
+ * Take a statement with "you <something> me" and transform it into "What makes
+ * you think that I <something> you?"
+ *
+ * @param statement the user statement, assumed to contain "you" followed by "me"
+ * @return the transformed statement
+ */
+
+public String transformYouMeStatement(String statement) {
+    	//Remove the final period, if there is one
+    	statement = statement.trim();
+    	String lastChar = statement.substring(statement.length() - 1);
+    	if (lastChar.equals(".")) {
+        	statement = statement.substring(0, statement.length() - 1);
+    	}
+
+    	int posOfYou = findKeyword(statement, "you", 0);
+    	int posOfMe  = findKeyword(statement, "me",  posOfYou + 3);
+
+    	String restOfStatement = statement.substring(posOfYou + 3, posOfMe).trim();
+    	return "What makes you think that I " + restOfStatement + " you?";
+}
+
+    
+    
+    /**
+     * Gives a response to a user statement
+     *
+     * @param statement the user statement
+     * @return a response based on the rules given
+     */
+    public String getResponse(String statement) {
+        String response = "";
+        
+        String statementblank = statement.trim();
+
+        
+        
+        if (findKeyword(statement, "mother", 0) >= 0 || findKeyword(statement, "father", 0) >= 0 || findKeyword(statement, "sister", 0) >= 0){
+            response = "Tell me more about your family.";
+        } else if (findKeyword(statement, "you", 0) >= 0 && findKeyword(statement, "you", 0) > findKeyword(statement, "me", 0)){
+            response = transformYouMeStatement(statement);
+        }
+        else if (findKeyword(statement, "weather", 0) >= 0){
+            response = "Weather is nice.";
+        } else if (findKeyword(statement, "dog", 0) >= 0 || findKeyword(statement, "cat", 0) >= 0){
+            response = "Tell me more about your pets.";
+        } else if (findKeyword(statement, "leon", 0) >= 0) {
+            response = "He sounds like a fun person.";
+        } else if (findKeyword(statement, "googoo", 0) >= 0) {
+            response = "are you a baby?";
+        } else if (statementblank.length() == 0 ) {
+            response = "Say something now.";
+        } else {
+            response = getRandomResponse();
+        }
+        
+        
+       if (statement.indexOf("dog") >= 0
+        || statement.indexOf("cat") >= 0) {
+            response = "Tell me more about your pets.";
+        } else if (statement.indexOf("leon") >= 0) {
+            response = "He sounds like a fun person.";
+        } else if (statementblank.length() == 0 ) {
+            response = "Say something now.";
+        } else if (statement.indexOf("googoo") >= 0) {
+            response = "are you a baby?";
+        } else if (statement.indexOf("woof") >= 0) {
+            response = "are you a dog?";
+        } else if (statement.indexOf("no") >= 0) {
+            response = "Why so negative?";
+        }
+        else {
+            response = getRandomResponse();
+        }
+
+        return response;
+    }
+
+    /**
+     * Pick a default response to use if nothing else fits.
+     *
+     * @return a non-committal string
+     */
+    private String getRandomResponse() {
+        final int NUMBER_OF_RESPONSES = 7;
+        double r = Math.random();
+        int whichResponse = (int) (r * NUMBER_OF_RESPONSES);
+
+        String response = "";
+
+        if (whichResponse == 0) {
+            response = "Interesting, tell me more.";
+        } else if (whichResponse == 1) {
+            response = "Hmmm.";
+        } else if (whichResponse == 2) {
+            response = "Do you really think so?";
+        } else if (whichResponse == 3) {
+            response = "You don't say.";
+        } else if (whichResponse == 4) {
+            response = "the weather is nice.";
+        } else if (whichResponse == 5) {
+            response = "woah thats awesome!";
+        } else if (whichResponse == 6) {
+            response = "i miss you too.";
+        }
+
+        return response;
+    }
+}
